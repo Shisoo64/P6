596681fc95517820fb4ef7c7fb692796ae7a142e
Romil <Shisoo64@gmail.com>
2020-09-27 20:32:07 +0200

    Check spawnpoint proximity

--- js/script.js
+++ js/script.js
@@ -7,6 +7,14 @@ addRandom(10, "rock");
 addRandom(4, "weapon");
 addRandom(2, "spawnPoint");
 
+while(checkSurround($('.cell.spawnPoint:first'), $('.cell.spawnPoint:last'), 1)){
+  $('.cell.spawnPoint').addClass('empty').removeClass('spawnPoint');
+  console.log("Spawnpoint too close");
+  addRandom(2, "spawnPoint");
+}
+
+
+
 
 function initGrid(x, y) {
   makeRows(x);
@@ -15,18 +23,29 @@ function initGrid(x, y) {
 
 function makeRows(rowNum) {
   for (r = 0; r < rowNum; r++) {
-    $('<div class="gridRow"></div>').appendTo('#container');
+    $('<div class="gridRow" data-row="'+ r +'" ></div>').appendTo('#container');
   };
 };
+
 
 function makeColumns(cellNum) {
   for (i = 0; i < rows.length; i++) {
     for (j = 0; j < cellNum; j++) {
-      $('<div class="cell empty"></div>').appendTo(rows[j]);
+      $('<div class="cell empty" data-x="'+ j +'" data-y="'+ i +'" ></div>').appendTo(rows[j]);
     };
   };
 };
 
+
+function checkSurround(first, second, perimeter) {
+
+  console.log(first.data("x"),first.data("y"));
+  console.log(second.data("x"),second.data("y"));
+
+  if(((second.data("x") <= first.data("x") + perimeter) && (second.data("x") >= first.data("x") - perimeter)) && ((second.data("y") <= first.data("y") + perimeter) && (second.data("y") >= first.data("y") - perimeter))){
+    return true;
+  }
+};
 
 
 function addRandom(num, className) {
