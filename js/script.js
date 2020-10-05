24d06b3a92f7fd5120504958102f67b16dfb3dea
Romil <Shisoo64@gmail.com>
2020-10-05 13:56:37 +0200

    Mode objet

--- js/script.js
+++ js/script.js
@@ -1,13 +1,73 @@
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 
+class Player {
+  constructor(x, y, health, weapon) {
+    this.x = x;
+    this.y = y;
+    this.health = 100;
+  }
 
-initGrid(10, 10);
+}
+
+class Weapon {
+  constructor(x, y, name, damage) {
+    this.x = x;
+    this.y = y;
+  }
+
+}
+
+class Cell {
+  constructor(x, y, status) {
+    this.x = x;
+    this.y = y;
+    $('<div class="cell empty" data-x="'+ x +'" data-y="'+ y +'" ></div>').appendTo(rows[x]);
+  }
+
+  checkSurround(second, perimeter) {
+    console.log(this.x,this.y);
+    console.log(second.data("x"),second.data("y"));
+    return(((second.data("x") <= this.x + perimeter) && (second.data("x") >= this.x - perimeter)) &&
+      ((second.data("y") <= this.y + perimeter) && (second.data("y") >= this.y - perimeter)));
+  };
+
+}
+
+class Grid {
+  constructor(hauteur, largeur, map) {
+    this.map = new Array(hauteur).fill(new Array(largeur));
+    // For pour création des Rows
+    for (var x = 0; x < hauteur; x++) {
+      $('<div class="gridRow" data-row="'+ x +'" ></div>').appendTo('#container');
+      // For pour ajout des cases a la Row
+      for (var y = 0; y < largeur; y++) {
+        this.map[x][y] = new Cell(x,y,"empty");
+      };
+    };
+  }
+
+  randomCell(status){
+    let randomX = Math.floor(Math.random() * this.map.length);
+    let randomY = Math.floor(Math.random() * this.map[randomX].length);
+    console.log(randomX, randomY);
+    return(randomX, randomY);
+  }
+
+}
+
+
+
+playGrid = new Grid(10,10);
 addRandom(10, "rock");
 addRandom(4, "weapon");
 addRandom(2, "spawnPoint");
+playGrid.randomCell();
 
-while(checkSurround($('.cell.spawnPoint:first'), $('.cell.spawnPoint:last'), 1)){
+
+
+// Check si les spawn sont a côté
+while(checkSurround($('.cell.spawnPoint:first'), $('.cell.spawnPoint:last'), 2)){
   $('.cell.spawnPoint').addClass('empty').removeClass('spawnPoint');
   console.log("Spawnpoint too close");
   addRandom(2, "spawnPoint");
@@ -15,36 +75,15 @@ while(checkSurround($('.cell.spawnPoint:first'), $('.cell.spawnPoint:last'), 1))
 
 
 
-
-function initGrid(x, y) {
-  makeRows(x);
-  makeColumns(y);
-}
-
-function makeRows(rowNum) {
-  for (r = 0; r < rowNum; r++) {
-    $('<div class="gridRow" data-row="'+ r +'" ></div>').appendTo('#container');
-  };
-};
-
-
-function makeColumns(cellNum) {
-  for (i = 0; i < rows.length; i++) {
-    for (j = 0; j < cellNum; j++) {
-      $('<div class="cell empty" data-x="'+ j +'" data-y="'+ i +'" ></div>').appendTo(rows[j]);
-    };
-  };
-};
-
-
 function checkSurround(first, second, perimeter) {
 
   console.log(first.data("x"),first.data("y"));
   console.log(second.data("x"),second.data("y"));
 
-  if(((second.data("x") <= first.data("x") + perimeter) && (second.data("x") >= first.data("x") - perimeter)) && ((second.data("y") <= first.data("y") + perimeter) && (second.data("y") >= first.data("y") - perimeter))){
-    return true;
-  }
+  return(((second.data("x") <= first.data("x") + perimeter) &&
+   (second.data("x") >= first.data("x") - perimeter)) &&
+    ((second.data("y") <= first.data("y") + perimeter) &&
+     (second.data("y") >= first.data("y") - perimeter)))
 };
 
 
